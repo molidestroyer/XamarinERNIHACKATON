28d1c3e8806092ce2004ed9ee3516e21d4f6bbb3
Oscar Vaquero <oscar.vaquero@contractors.roche.com>
2017-11-04 09:49:50 +0100

    Add camera

--- PokemonRecognition.ViewModels/MainPageViewModel.cs
+++ PokemonRecognition.ViewModels/MainPageViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Plugin.Media;
 using Xamarin.Forms;
 
 namespace PokemonRecognition.ViewModels
@@ -12,6 +13,7 @@ namespace PokemonRecognition.ViewModels
     public class MainPageViewModel:BaseViewModel
     {
         private ICommand _clickCameraCommand;
+        private Image _image;
         public ICommand SelectedFieldCommand
         {
             get { return _clickCameraCommand = _clickCameraCommand ?? new Command(onClickCameraCommand); }
@@ -29,6 +31,36 @@ namespace PokemonRecognition.ViewModels
             var result = service.GetHandwrittenTextFromImage(new byte[8]);
         }
 
+
+        private async Task TakePicture()
+        {
+
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                //DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            // await DisplayAlert("File Location", file.Path, "OK");
+            _image = new Image();
+            _image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
 
 
     }
