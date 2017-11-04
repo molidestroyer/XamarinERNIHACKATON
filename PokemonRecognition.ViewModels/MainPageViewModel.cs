25f1eff980e98a619a8c88cf635ef841fa4485b0
Miguel Molina <miguel.molina@kaba.com>
2017-11-04 09:48:59 +0100

    working version

--- PokemonRecognition.ViewModels/MainPageViewModel.cs
+++ PokemonRecognition.ViewModels/MainPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using PokemonRecognition.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace PokemonRecognition.ViewModels
     public class MainPageViewModel:BaseViewModel
     {
         private ICommand _clickCameraCommand;
-        public ICommand SelectedFieldCommand
+        public ICommand ClickCameraCommand
         {
             get { return _clickCameraCommand = _clickCameraCommand ?? new Command(onClickCameraCommand); }
         }
@@ -23,10 +24,10 @@ namespace PokemonRecognition.ViewModels
 
         }
 
-        private void onClickCameraCommand(object obj)
+        private async void onClickCameraCommand(object obj)
         {
             var service = new TextRecognitionService();
-            var result = service.GetHandwrittenTextFromImage(new byte[8]);
+            var result = await service.GetHandwrittenTextFromImage(Stream.Null);
         }
 
 
